80094c12067c42fe116a055aa6dcde43cc41fe38
Kyle Gough <kylegough98@gmail.com>
2020-05-23 11:32:00 +0100

    Changed procfile

--- src/index.js
+++ src/index.js
@@ -88,6 +88,7 @@ const theme = responsiveFontSizes(createMuiTheme({
 
 //TODO "react-scripts start"
 //https://github.com/mars/create-react-app-buildpack.git#v6.0.0
+//web: bin/boot
 
 class App extends React.Component {
   
