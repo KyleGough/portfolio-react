5869b26bc55712d0aa14d24132e5066462c7dfd2
Kyle Gough <kylegough98@gmail.com>
2020-05-27 16:16:42 +0100

    Added text compression

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser')
 const path = require('path');
+const compression = require('compression');
 const app = express();
+
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
