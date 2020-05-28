61fcd8a082e76fd2695b7d85e429f68d7960366d
Kyle Gough <kylegough98@gmail.com>
2020-05-28 21:01:44 +0100

    Organised homepage

--- src/HomePage.js
+++ src/HomePage.js
@@ -11,6 +11,9 @@ import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 import commonStyles from './Styles';
 import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+import AvatarGroup from '@material-ui/lab/AvatarGroup';
+import Paper from '@material-ui/core/Paper';
 
 
 const pageStyles = theme => ({
@@ -51,7 +54,51 @@ const pageStyles = theme => ({
   linkDesc: {
     fontFamily: theme.font.primary,
     fontWeight: 200,
-    marginTop: "1.25em"
+    marginTop: "1.25em",
+    textAlign: "center"
+  },
+  linkSection: {
+    paddingBottom: "4em",
+    paddingTop: "4em",
+    fontSize: "15px",
+    fontFamily: theme.font.primary
+  },
+  flex: {
+    display: "flex",
+    textAlign: "center",
+    alignItems: "center",
+
+  },
+  avatar: {
+    textAlign: "center",
+    width: 200,
+    height: 200,
+    marginTop: "2em",
+    [theme.breakpoints.down("sm")]: {
+      marginTop: 0
+    }
+  },
+  avatarList: {
+    marginBottom: "1em",
+    marginTop: "1em",
+    border: "2px solid " + theme.palette.textSecondary,
+    width: 100,
+    height: 100,
+    "&:hover": {
+      zIndex: 1000
+    }
+  },
+  avatarGroup: {
+    marginLeft: "calc(50% - 75px)"
+  },
+  linkColumn: {
+    display: "block",
+    textAlign: "center",
+    marginTop: "4em",
+    marginBottom: "4em"
+  },
+  paper: {
+    padding: "2em"
   }
 });
 
@@ -89,8 +136,7 @@ class HomePage extends React.Component {
     return (
       <Container className={classes.root} maxWidth="lg">        
                 
-        <div className={classes.section}>          
-         
+        <div className={classes.section}>           
           <Grid container spacing={3}>
             <Grid container item xs={12} sm={7}>
               <Typography className={classes.title} variant="h3" component="h1">Portfolio</Typography>
@@ -101,28 +147,39 @@ class HomePage extends React.Component {
                 of this website from PHP to React and a logical Sudoku solver written in Python.
               </p>                   
             </Grid>
-            <Grid className={classes.columnBlock} container item xs={12} sm={5}>
-             
-            </Grid>
-          </Grid>  
-
-          
-          <Grid className={classes.gridLink} container spacing={3}>
-            <Grid container item xs={12} sm={6}>
-              <Link to="/projects">
-                <Button className={classes.linkButton}>Project List</Button>
-              </Link>
-              <p className={classes.linkDesc}>View a list of all my programming projects</p>
-            </Grid>
-            <Grid className={classes.columnBlock} container item xs={12} sm={6}>
-              <Link to="/about">
-                <Button className={classes.linkButton}>About Me</Button>
-              </Link>  
-              <p className={classes.linkDesc}>View my skills and additional information</p>
+            <Grid className={classes.columnBlock} container item xs={12} sm={4} alignItems="center" justify="center">
+              <Avatar className={classes.avatar} alt="Avatar" src="/img/cave-exploration1.jpg" />
             </Grid>
           </Grid>  
         </div>
 
+        <Grid container spacing={0} alignItems="center" justify="center">
+          <Grid className={classes.linkColumn} container item xs={12} md={4} alignItems="center" justify="center">
+            <Paper className={classes.paper} elevation={1}>
+              <Link to="/projects">
+                <Button className={classes.linkButton}>Project List</Button>
+              </Link>
+              <p className={classes.linkDesc}>View a list of all my programming projects</p>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} md={4} alignItems="center" justify="center">
+            <AvatarGroup className={classes.avatarGroup} spacing={50} max={4}>
+              <Avatar className={classes.avatarList} alt={imageShowcase[0].title} src={imageShowcase[0].src} />
+              <Avatar className={classes.avatarList} alt={imageShowcase[1].title} src={imageShowcase[1].src} />
+              <Avatar className={classes.avatarList} alt={imageShowcase[2].title} src={imageShowcase[2].src} />
+              <Avatar className={classes.avatarList} alt={imageShowcase[3].title} src={imageShowcase[3].src} />
+            </AvatarGroup>    
+          </Grid>  
+          <Grid className={classes.linkColumn} container item xs={12} md={4} alignItems="center" justify="center">
+            <Paper className={classes.paper} elevation={1}>    
+              <Link to="/about">
+                <Button className={classes.linkButton}>About Me</Button>
+              </Link>  
+              <p className={classes.linkDesc}>View my skills and additional information</p> 
+            </Paper>
+          </Grid>
+        </Grid> 
+       
         <Divider />
 
         <div className={classes.imgShowcase}>
